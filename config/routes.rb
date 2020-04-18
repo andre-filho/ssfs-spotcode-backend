30b7c4eb55189feb5ec0908db9380bccfd73cd99
Andre Filho <andre.filho001@outlook.com>
2020-04-18 15:58:37 -0300

    chore(devise): adds routes and sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :songs
+  devise_for :users,
+             controllers: {
+               sessions: 'sessions',
+               registrations: 'registrations'
+             }  resources :songs
   resources :albums
   resources :categories
   resources :artists
